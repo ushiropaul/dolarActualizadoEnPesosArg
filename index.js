6ed26e097867be65efd8c871d937dc153c58b438
Paul Ushiro <153045119+ushiropaul@users.noreply.github.com>
2024-11-07 23:23:48 +0000

    probando render de time

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const USDtypes = ["oficial", "blue", "cripto", "bolsa" ,"contadoconliqui", "mayorista", "tarjeta"];
-
+const USDtypes = ["oficial", "blue", "cripto", "bolsa", "contadoconliqui", "mayorista", "tarjeta"];
 const containerCard = document.getElementById("containerCard");
-
 
 USDtypes.forEach(type => {
     fetch(`https://dolarapi.com/v1/dolares/${type}`)
@@ -13,6 +11,18 @@ USDtypes.forEach(type => {
         .catch(error => console.error('Error fetching data:', error));
 });
 
+function formatDate(fechaISO) {
+    const fecha = new Date(fechaISO);
+    const opciones = {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    };
+    return fecha.toLocaleString('es-AR', opciones);
+}
 
 function displayValueUSDblue(data, type) {
     const usdCard = document.createElement('div');
@@ -24,9 +34,8 @@ function displayValueUSDblue(data, type) {
             <li><strong>Valor de compra:</strong> <span>$${data.compra} en pesos ARG</span></li>
             <li><strong>Valor promedio:</strong> <span>$${((data.venta + data.compra).toFixed(2) / 2)} en pesos ARG</span></li>
             <li><strong>Valor de venta:</strong> <span>$${data.venta} en pesos ARG</span></li>
-            <li><strong>Fecha de actualización:</strong> <span>${data.fechaActualizacion}</span></li>
+            <li><strong>Fecha de actualización:</strong> <span>${formatDate(data.fechaActualizacion)}</span></li>
         </ul>
     `;
     containerCard.appendChild(usdCard);
 }
-
